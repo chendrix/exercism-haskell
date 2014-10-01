da1381653e7e42c147aae344ce0bde2882dce1b2
Chris Hendrix <cah276@cornell.edu>
2014-09-30 19:01:44 -0700

    Second iteration of Bob, replacing a let with a where

--- bob/Bob.hs
+++ bob/Bob.hs
@@ -2,19 +2,19 @@ module Bob
 ( responseFor
 ) where
   
-  import Data.Char
+import Data.Char
+
+responseFor :: String -> String
+responseFor saying
+  | all isSpace saying = "Fine. Be that way!"
+  | allUpper saying = "Whoa, chill out!" 
+  | saying `endsWith` '?' = "Sure."
+  | otherwise = "Whatever."
   
-  responseFor :: String -> String
-  responseFor saying
-    | all isSpace saying = "Fine. Be that way!"
-    | allUpper saying = "Whoa, chill out!" 
-    | saying `endsWith` '?' = "Sure."
-    | otherwise = "Whatever."
-    
-  endsWith :: String -> Char -> Bool
-  endsWith str char = last str == char
-  
-  allUpper :: String -> Bool
-  allUpper str = 
-    let alphaStr = filter isAlpha str 
-    in not (null alphaStr) && all isUpper alphaStr
+endsWith :: String -> Char -> Bool
+endsWith str char = last str == char
+
+allUpper :: String -> Bool
+allUpper str = 
+  not (null alphaStr) && all isUpper alphaStr
+  where alphaStr = filter isAlpha str 
