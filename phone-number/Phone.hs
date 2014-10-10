a570be50300fe576ff755c5b8b21cceb127aff1c
Chris Hendrix <cah276@cornell.edu>
2014-10-09 21:54:05 -0700

    Using splitAt instead of drop and take

--- phone-number/Phone.hs
+++ phone-number/Phone.hs
@@ -9,19 +9,12 @@ import Text.Printf
 
 prettyPrint :: String -> String
 prettyPrint unformattedNumber = 
-  printf "(%s) %s-%s" area first second
-  where area   = areaCode unformattedNumber
-        first  = firstPart unformattedNumber
-        second = secondPart unformattedNumber
+  printf "(%s) %s-%s" area prefix lineNumber
+  where (area, rest) = splitAt 3 (number unformattedNumber)
+        (prefix, lineNumber) = splitAt 3 rest
 
 areaCode :: String -> String
 areaCode phoneNumber = take 3 $ number phoneNumber
-
-firstPart :: String -> String
-firstPart phoneNumber = drop 3 $ take 6 $ number phoneNumber
-
-secondPart :: String -> String
-secondPart phoneNumber = drop 3 $ drop 3 $ number phoneNumber
 
 number :: String -> String
 number str 
