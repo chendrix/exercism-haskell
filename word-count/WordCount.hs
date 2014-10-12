be94977ee7e3e16c5a49cbddf78241802b2cd332
Chris Hendrix <cah276@cornell.edu>
2014-10-11 21:36:08 -0700

    WordCount: Fix a few more tests by filtering for only alphaNum and separators

--- word-count/WordCount.hs
+++ word-count/WordCount.hs
@@ -3,6 +3,12 @@ module WordCount
 ) where
   
 import Data.Map as M
+import Data.List as L
+import Data.Char as C
 
 wordCount :: String -> M.Map String Int
-wordCount s = M.fromList $ zip (words s) (repeat 1)
+wordCount phrase = L.foldr (\e m -> insertWith (+) e 1 m) startingMap tokens
+  where isValidChar n = C.isSeparator n || C.isAlphaNum n
+        s = L.filter isValidChar phrase
+        tokens = words s
+        startingMap = M.empty
