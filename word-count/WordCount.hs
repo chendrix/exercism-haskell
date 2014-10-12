71428687c62c8d6223ab8768cdc3ef7817540326
Chris Hendrix <cah276@cornell.edu>
2014-10-11 21:42:40 -0700

    WordCount: Finished. Iteration 1

--- word-count/WordCount.hs
+++ word-count/WordCount.hs
@@ -8,9 +8,11 @@ import Data.Char as C
 
 wordCount :: String -> M.Map String Int
 wordCount phrase = L.foldr (\e m -> insertWith (+) e 1 m) startingMap tokens
-  where isValidChar n = C.isSeparator n || C.isAlphaNum n
-        downcase = L.map C.toLower
-        s = L.filter isValidChar (downcase phrase)
-        tokens = words s
+  where tokens = words s
         startingMap = M.empty
+        s = L.filter isValidChar (downcase spacefiedPhrase)
+        isValidChar n = C.isSeparator n || C.isAlphaNum n
+        downcase = L.map C.toLower
+        spacefiedPhrase = L.map (\n -> if C.isPunctuation n then ' ' else n) phrase 
+                
         
