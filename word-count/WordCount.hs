75011527c3aa187922ddd2aa5f73e21ec6fa87d5
Chris Hendrix <cah276@cornell.edu>
2014-10-11 21:37:58 -0700

    WordCount: Fix more tests by downcasing all phrases first

--- word-count/WordCount.hs
+++ word-count/WordCount.hs
@@ -9,6 +9,8 @@ import Data.Char as C
 wordCount :: String -> M.Map String Int
 wordCount phrase = L.foldr (\e m -> insertWith (+) e 1 m) startingMap tokens
   where isValidChar n = C.isSeparator n || C.isAlphaNum n
-        s = L.filter isValidChar phrase
+        downcase = L.map C.toLower
+        s = L.filter isValidChar (downcase phrase)
         tokens = words s
         startingMap = M.empty
+        
