0c9db38f7c2a2dc885704f2bb48c547cf3d685c9
Chris Hendrix <cah276@cornell.edu>
2014-10-12 23:58:36 -0700

    RobotName: Third Iteration, separated the monadic randomName from the deterministic one

--- robot-name/Robot.hs
+++ robot-name/Robot.hs
@@ -18,13 +18,15 @@ mkRobot = do
 
 randomName :: IO Name
 randomName = do
-  g <- getStdGen
-  h <- newStdGen
-  let randomChars = randomRs ('A', 'Z') g
-      randomNumbers = randomRs (0, 9) h :: [Int]
-      twoChars = take 2 randomChars
-      threeDigitString = map intToDigit $ take 3 randomNumbers
-  return $ twoChars ++ threeDigitString 
+  g <- newStdGen
+  return $ _randomName g
+
+_randomName :: StdGen -> Name
+_randomName g = twoChars ++ threeDigitString 
+  where randomChars = randomRs ('A', 'Z') g
+        randomNumbers = randomRs (0, 9) g :: [Int]
+        twoChars = take 2 randomChars
+        threeDigitString = map intToDigit $ take 3 randomNumbers
 
 robotName :: IORef Robot -> IO Name
 robotName ref = do
