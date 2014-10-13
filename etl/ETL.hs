6f76ea490e497226786d153f2ee3462c801f7221
Chris Hendrix <cah276@cornell.edu>
2014-10-12 22:36:26 -0700

    ETL: Second iteration

--- etl/ETL.hs
+++ etl/ETL.hs
@@ -4,6 +4,7 @@ module ETL
 
 import qualified Data.Map.Strict as M
 import Data.Char
+import qualified Data.List as L
 
 type Score = Int
 type Letter = String
@@ -12,8 +13,8 @@ type LegacySystem = M.Map Score [Letter]
 type NewSystem = M.Map Letter Score
 
 transform :: LegacySystem -> NewSystem
-transform = M.foldrWithKey' addLettersScores M.empty
-  where addLettersScores score letters scoreList = foldr (score `addLetterScore`) scoreList letters
+transform = M.foldlWithKey' addLettersScores M.empty
+  where addLettersScores scoreList score = L.foldl' (flip (addLetterScore score)) scoreList
         addLetterScore score letter = M.insert (downcase letter) score
 
 downcase :: Letter -> Letter
